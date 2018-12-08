60522bed93fde5e8617eebb4b19a5d0bbd086a4e
qtrinh128 <quangtrinh128@gmail.com>
2018-12-08 15:56:08 +0700

    Add project files.

---
+++ AmazonApp/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace AmazonApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClick = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.RichTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClick
+            // 
+            this.btnClick.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClick.Location = new System.Drawing.Point(557, 286);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(161, 54);
+            this.btnClick.TabIndex = 1;
+            this.btnClick.Text = "Run";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(12, 68);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(170, 212);
+            this.txtResult.TabIndex = 2;
+            this.txtResult.Text = "";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(239, 68);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(479, 212);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ID seller";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(234, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "List messages";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 366);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnClick);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "App Amazon";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.RichTextBox txtResult;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ AmazonApp/Form1.cs
@@ -0,0 +1,137 @@
+﻿using Chilkat;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AmazonApp
+{
+    public partial class Form1 : Form
+    {
+        void writeLog(string log)
+        {
+            using (StreamWriter sw = new StreamWriter("LogAmazon.txt", true))
+            {
+                sw.WriteLine(log);
+            }
+        }
+        void createAccount()
+        {
+            const string URL_LOGIN = "https://www.amazon.com/ap/register?_encoding=UTF8&openid.assoc_handle=usflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26ref_%3Dnav_custrec_newcust";
+            const string URL_LOGIN_SUCCESS = "https://www.amazon.com/gp/yourstore/home?ie=UTF8&action=sign-out&path=%2Fgp%2Fyourstore%2Fhome&ref_=nav_youraccount_signout&signIn=1&useRedirectOnSuccess=1&claim_type=EmailAddress&new_account=1&";
+            const string URL_REGISTER = "https://www.amazon.com/ap/register";
+            Bogus.Faker faker = new Bogus.Faker();
+            string fakerName = faker.Name.FullName();
+            string fakerPassword = faker.Internet.Password();
+            string fakerEmail = faker.Internet.Email();
+            //string fakerUserAgent = faker.Internet.UserAgent();
+            string fakerUserAgent = $"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/{faker.Random.Int(100000, 999999)}.{faker.Random.Int(50, 999)} (KHTML, like Gecko) Chrome/{faker.Random.Int(50, 999)}.{faker.Random.Int(50, 999)}.{faker.Random.Int(50, 999)}.{faker.Random.Int(50, 999)} Safari/{faker.Random.Int(50, 999)}.{faker.Random.Int(50, 999)}";
+            string register = "REGISTER";
+
+            Http http = new Http();
+            bool success = http.UnlockComponent("Anything for 30-day trial");
+            if (success != true)
+            {
+                txtResult.Text = http.LastErrorText.ToString();
+                return;
+            }
+
+            // Using sock 5
+            http.SocksVersion = 5;
+            //  Set the SocksHostname to the SOCKS proxy domain name or IP address,
+            //  which may be IPv4 (dotted notation) or IPv6.
+            http.SocksHostname = "103.240.161.109";
+            http.SocksPort = 6667;
+
+            http.CookieDir = "memory";
+            http.SaveCookies = true;
+            http.SendCookies = true;
+            http.UserAgent = fakerUserAgent;
+
+            string showMyIP = http.QuickGetStr("http://ip-api.com/json");
+            if (true)
+            {
+
+            }
+
+            string htmlLogin = http.QuickGetStr(URL_LOGIN);
+
+            string appActionToken = Regex.Match(htmlLogin, "name=\"appActionToken\" value=\"(.+?)\" />").Groups[1].Value.Trim();
+            string openidReturnTo = Regex.Match(htmlLogin, "name=\"openid.return_to\" value=\"(.+?)\" />").Groups[1].Value.Trim();
+            string prevRID = Regex.Match(htmlLogin, "name=\"prevRID\" value=\"(.+?)\" />").Groups[1].Value.Trim();
+            string workflowState = Regex.Match(htmlLogin, "name=\"workflowState\" value=\"(.+?)\" />").Groups[1].Value.Trim();
+
+            HttpRequest requestLogin = new HttpRequest();
+            requestLogin.AddParam("appActionToken", appActionToken);
+            requestLogin.AddParam("appAction", register);
+            requestLogin.AddParam("openid.return_to", openidReturnTo);
+            requestLogin.AddParam("prevRID	", prevRID);
+            requestLogin.AddParam("workflowState", workflowState);
+            requestLogin.AddParam("customerName", fakerName);
+            requestLogin.AddParam("email", fakerEmail);
+            requestLogin.AddParam("password", fakerPassword);
+            requestLogin.AddParam("passwordCheck", fakerPassword);
+            HttpResponse responseLogin = http.PostUrlEncoded(URL_REGISTER, requestLogin);
+            if (responseLogin == null)
+            {
+                return;
+            }
+            string htmlLoginSuccess = http.QuickGetStr(URL_LOGIN_SUCCESS);
+            string checkEmailExists = Regex.Match(htmlLoginSuccess, "<title dir=\"ltr\">(.+?)</title>").Groups[1].Value.Trim();
+            bool check = Equals(checkEmailExists, "Amazon Sign In");
+            if (check)
+            {
+                txtResult.Invoke(new MethodInvoker(delegate() {
+                    txtResult.Text += "Reg error\n";
+                }));
+               
+                return;
+            }
+            sendMess(http);
+            string log = fakerName + "|" + fakerEmail + "|" + fakerPassword;
+            writeLog(log);
+        }
+        void sendMess(Http http)
+        {
+            while (true)
+            {
+                HttpRequest requestSendMess = new HttpRequest();
+                requestSendMess.AddParam("comment", " hello hellohello hellohello hellohello hellohello hello hello hello hello ");
+                string idSeler = "ANSF0RE9FUP82";
+                HttpResponse responseLogin = http.PostUrlEncoded($"https://www.amazon.com/ss/help/contact/submitMessage?writeButton=Gửi+truy+vấn&subject=3&orderID=&sellerID={idSeler}&asin=&marketplaceID=ATVPDKIKX0DER&language=en_US", requestSendMess);
+                if (true)
+                {
+
+                }
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            //CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            List<string> lstMessage = new List<string>(richTextBox1.Text.Split('\n'));
+
+            for (int i = 0; i < 8; i++)
+            {
+                ThreadStart ts = new ThreadStart(createAccount);
+                Thread th = new Thread(ts);
+                th.IsBackground = true;
+                th.Start();
+            }
+       
+            ////createAccount();
+        }
+    }
+}
