ad5b138ab000d992e38d16d0840b6fb7a9052bab
qtrinh128 <quangtrinh128@gmail.com>
2018-12-13 03:06:36 +0700

    Them tinh nang dang ky va gui da luong

--- AmazonApp/Form1.Designer.cs
+++ AmazonApp/Form1.Designer.cs
@@ -29,44 +29,48 @@
         private void InitializeComponent()
         {
             this.btnClick = new System.Windows.Forms.Button();
-            this.txtResult = new System.Windows.Forms.RichTextBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.txtListID = new System.Windows.Forms.RichTextBox();
+            this.txtListMessage = new System.Windows.Forms.RichTextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.txtListSock = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblSockLive = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // btnClick
             // 
             this.btnClick.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClick.Location = new System.Drawing.Point(557, 286);
+            this.btnClick.Location = new System.Drawing.Point(12, 230);
             this.btnClick.Name = "btnClick";
-            this.btnClick.Size = new System.Drawing.Size(161, 54);
+            this.btnClick.Size = new System.Drawing.Size(170, 35);
             this.btnClick.TabIndex = 1;
             this.btnClick.Text = "Run";
             this.btnClick.UseVisualStyleBackColor = true;
             this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
             // 
-            // txtResult
+            // txtListID
             // 
-            this.txtResult.Location = new System.Drawing.Point(12, 68);
-            this.txtResult.Name = "txtResult";
-            this.txtResult.Size = new System.Drawing.Size(170, 212);
-            this.txtResult.TabIndex = 2;
-            this.txtResult.Text = "";
+            this.txtListID.Location = new System.Drawing.Point(188, 68);
+            this.txtListID.Name = "txtListID";
+            this.txtListID.Size = new System.Drawing.Size(170, 156);
+            this.txtListID.TabIndex = 2;
+            this.txtListID.Text = "";
             // 
-            // richTextBox1
+            // txtListMessage
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(239, 68);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(479, 212);
-            this.richTextBox1.TabIndex = 2;
-            this.richTextBox1.Text = "";
+            this.txtListMessage.Location = new System.Drawing.Point(364, 68);
+            this.txtListMessage.Name = "txtListMessage";
+            this.txtListMessage.Size = new System.Drawing.Size(641, 156);
+            this.txtListMessage.TabIndex = 2;
+            this.txtListMessage.Text = "";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(13, 27);
+            this.label1.Location = new System.Drawing.Point(183, 27);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(99, 25);
             this.label1.TabIndex = 3;
@@ -76,21 +80,61 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(234, 27);
+            this.label2.Location = new System.Drawing.Point(359, 27);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(163, 25);
             this.label2.TabIndex = 3;
             this.label2.Text = "List messages";
             // 
+            // txtListSock
+            // 
+            this.txtListSock.Location = new System.Drawing.Point(12, 68);
+            this.txtListSock.Name = "txtListSock";
+            this.txtListSock.Size = new System.Drawing.Size(170, 156);
+            this.txtListSock.TabIndex = 2;
+            this.txtListSock.Text = "";
+            this.txtListSock.TextChanged += new System.EventHandler(this.txtListSock_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(7, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "List sock";
+            // 
+            // lblSockLive
+            // 
+            this.lblSockLive.AutoSize = true;
+            this.lblSockLive.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSockLive.Location = new System.Drawing.Point(110, 27);
+            this.lblSockLive.Name = "lblSockLive";
+            this.lblSockLive.Size = new System.Drawing.Size(0, 25);
+            this.lblSockLive.TabIndex = 4;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(12, 332);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(993, 122);
+            this.txtResult.TabIndex = 5;
+            this.txtResult.Text = "";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(755, 366);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.richTextBox1);
+            this.ClientSize = new System.Drawing.Size(1017, 466);
             this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.lblSockLive);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtListMessage);
+            this.Controls.Add(this.txtListSock);
+            this.Controls.Add(this.txtListID);
             this.Controls.Add(this.btnClick);
             this.MaximizeBox = false;
             this.Name = "Form1";
@@ -103,10 +147,14 @@
 
         #endregion
         private System.Windows.Forms.Button btnClick;
-        private System.Windows.Forms.RichTextBox txtResult;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox txtListID;
+        private System.Windows.Forms.RichTextBox txtListMessage;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox txtListSock;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblSockLive;
+        private System.Windows.Forms.RichTextBox txtResult;
     }
 }
 
--- AmazonApp/Form1.cs
+++ AmazonApp/Form1.cs
@@ -1,137 +1,237 @@
 ﻿using Chilkat;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AmazonApp
 {
     public partial class Form1 : Form
     {
-        void writeLog(string log)
+        void PrintLog(string s, Color color)
+        {
+            DateTime date = DateTime.Now;
+            txtResult.Invoke(new MethodInvoker(() =>
+            {
+                txtResult.SelectionStart = txtResult.TextLength;
+                txtResult.SelectionLength = 0;
+
+                txtResult.SelectionColor = color;
+                txtResult.AppendText($"{date.ToString()}: {s} {Environment.NewLine}");
+                txtResult.SelectionColor = txtResult.ForeColor;
+            }));
+        }
+        string GetUserAgent()
+        {
+            Bogus.Faker faker = new Bogus.Faker();
+            string userAgentChorme = $"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/{faker.Random.Int(100000, 999999)}.{faker.Random.Int(50, 999)} (KHTML, like Gecko) Chrome/{faker.Random.Int(50, 999)}.{faker.Random.Int(50, 999)}.{faker.Random.Int(50, 999)}.{faker.Random.Int(50, 999)} Safari/{faker.Random.Int(50, 999)}.{faker.Random.Int(50, 999)}";
+            string userAgentFirefox = $"Mozilla/5.0 (Windows NT 5.1; rv:7.0.1) Gecko/20100101 Firefox/{faker.Random.Int(100, 1000)}.{faker.Random.Int(10, 100)}.{faker.Random.Int(100, 1000)}";
+            string userAgentSafari = $"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/{faker.Random.Int(1000, 10000)}.{faker.Random.Int(100, 1000)}.{faker.Random.Int(10, 100)} (KHTML, like Gecko) Version/{faker.Random.Int(1, 10)}.{faker.Random.Int(1, 10)}.{faker.Random.Int(1, 10)} Safari/7046A194A";
+            List<string> userAgent = new List<string>();
+            userAgent.Add(userAgentChorme);
+            userAgent.Add(userAgentFirefox);
+            userAgent.Add(userAgentSafari);
+            int rd = faker.Random.Int(0, userAgent.Count - 1);
+            return userAgent[rd];
+        }
+        /// <summary>
+        /// write file log account reg success
+        /// </summary>
+        /// <param name="log"></param>
+        void WriteLog(string log)
         {
             using (StreamWriter sw = new StreamWriter("LogAmazon.txt", true))
             {
                 sw.WriteLine(log);
             }
         }
-        void createAccount()
+        /// <summary>
+        /// create List<string> from richtextbox
+        /// </summary>
+        /// <param name="txt"></param>
+        /// <returns></returns>
+        List<string> CreateListFromRichTextBox(RichTextBox txt)
         {
-            const string URL_LOGIN = "https://www.amazon.com/ap/register?_encoding=UTF8&openid.assoc_handle=usflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26ref_%3Dnav_custrec_newcust";
+            List<string> list = new List<string>(txt.Text.Split('\n'));
+            return list;
+        }
+        /// <summary>
+        /// Create string from list<string>
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        string CreateString(List<string> list)
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            foreach (string item in list)
+            {
+                sb.Append(item);
+                sb.AppendLine();
+            }
+            return sb.ToString();
+        }
+        /// <summary>
+        /// Get sock live add to list
+        /// </summary>
+        /// <returns></returns>
+        void GetListSock()
+        {
+            List<string> listSockCheck = CreateListFromRichTextBox(txtListSock);
+            //var cts = new CancellationTokenSource();
+            foreach (var item in listSockCheck)
+            {
+                ThreadPool.QueueUserWorkItem(o =>
+                {
+                    //CancellationToken token = (CancellationToken)o;
+                    //while (!token.IsCancellationRequested)
+                    //{
+                    Http http = new Http();
+                    bool success = http.UnlockComponent("Anything for 30-day trial");
+                    if (success != true)
+                    {
+                        MessageBox.Show("Vui lòng thử lại sau!!!", "Thông  báo");
+                        return;
+                    }
+                    http.SocksVersion = 5;
+                    string[] sock = item.Split(':');
+                    http.SocksHostname = sock[0];
+                    http.SocksPort = int.Parse(sock[1]);
+                    PrintLog($"Checking {sock[0]} ...", Color.Aqua);
+                    string showMyIP = http.QuickGetStr("https://api.ipify.org/?format=json");
+                    if (String.IsNullOrEmpty(showMyIP))
+                    {
+                        listSockCheck.Remove(item);
+                        PrintLog($"{sock[0]} not working", Color.Red);
+                        txtListSock.Invoke(new MethodInvoker(delegate ()
+                        {
+                            txtListSock.Text = CreateString(listSockCheck);
+                        }));
+                    }
+                    else
+                    {
+                        CreateAccount(http);
+                    }
+                    //}
+                }); //cts.Token);
+            }
+        }
+
+        void CreateAccount(Http http)
+        {
+            const string GET_URL_REGISTER = "https://www.amazon.com/ap/register?_encoding=UTF8&openid.assoc_handle=usflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26ref_%3Dnav_custrec_newcust";
             const string URL_LOGIN_SUCCESS = "https://www.amazon.com/gp/yourstore/home?ie=UTF8&action=sign-out&path=%2Fgp%2Fyourstore%2Fhome&ref_=nav_youraccount_signout&signIn=1&useRedirectOnSuccess=1&claim_type=EmailAddress&new_account=1&";
-            const string URL_REGISTER = "https://www.amazon.com/ap/register";
+            const string POST_URL_REGISTER = "https://www.amazon.com/ap/register";
             Bogus.Faker faker = new Bogus.Faker();
             string fakerName = faker.Name.FullName();
             string fakerPassword = faker.Internet.Password();
             string fakerEmail = faker.Internet.Email();
-            //string fakerUserAgent = faker.Internet.UserAgent();
-            string fakerUserAgent = $"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/{faker.Random.Int(100000, 999999)}.{faker.Random.Int(50, 999)} (KHTML, like Gecko) Chrome/{faker.Random.Int(50, 999)}.{faker.Random.Int(50, 999)}.{faker.Random.Int(50, 999)}.{faker.Random.Int(50, 999)} Safari/{faker.Random.Int(50, 999)}.{faker.Random.Int(50, 999)}";
             string register = "REGISTER";
-
-            Http http = new Http();
-            bool success = http.UnlockComponent("Anything for 30-day trial");
-            if (success != true)
-            {
-                txtResult.Text = http.LastErrorText.ToString();
-                return;
-            }
-
-            // Using sock 5
-            http.SocksVersion = 5;
-            //  Set the SocksHostname to the SOCKS proxy domain name or IP address,
-            //  which may be IPv4 (dotted notation) or IPv6.
-            http.SocksHostname = "103.240.161.109";
-            http.SocksPort = 6667;
 
             http.CookieDir = "memory";
             http.SaveCookies = true;
             http.SendCookies = true;
-            http.UserAgent = fakerUserAgent;
+            http.UserAgent = GetUserAgent().ToString();
 
-            string showMyIP = http.QuickGetStr("http://ip-api.com/json");
-            if (true)
-            {
-
-            }
-
-            string htmlLogin = http.QuickGetStr(URL_LOGIN);
-
-            string appActionToken = Regex.Match(htmlLogin, "name=\"appActionToken\" value=\"(.+?)\" />").Groups[1].Value.Trim();
-            string openidReturnTo = Regex.Match(htmlLogin, "name=\"openid.return_to\" value=\"(.+?)\" />").Groups[1].Value.Trim();
-            string prevRID = Regex.Match(htmlLogin, "name=\"prevRID\" value=\"(.+?)\" />").Groups[1].Value.Trim();
-            string workflowState = Regex.Match(htmlLogin, "name=\"workflowState\" value=\"(.+?)\" />").Groups[1].Value.Trim();
-
-            HttpRequest requestLogin = new HttpRequest();
-            requestLogin.AddParam("appActionToken", appActionToken);
-            requestLogin.AddParam("appAction", register);
-            requestLogin.AddParam("openid.return_to", openidReturnTo);
-            requestLogin.AddParam("prevRID	", prevRID);
-            requestLogin.AddParam("workflowState", workflowState);
-            requestLogin.AddParam("customerName", fakerName);
-            requestLogin.AddParam("email", fakerEmail);
-            requestLogin.AddParam("password", fakerPassword);
-            requestLogin.AddParam("passwordCheck", fakerPassword);
-            HttpResponse responseLogin = http.PostUrlEncoded(URL_REGISTER, requestLogin);
-            if (responseLogin == null)
+            string htmlLogin = http.QuickGetStr(GET_URL_REGISTER);
+            if (String.IsNullOrEmpty(htmlLogin))
             {
                 return;
             }
-            string htmlLoginSuccess = http.QuickGetStr(URL_LOGIN_SUCCESS);
-            string checkEmailExists = Regex.Match(htmlLoginSuccess, "<title dir=\"ltr\">(.+?)</title>").Groups[1].Value.Trim();
-            bool check = Equals(checkEmailExists, "Amazon Sign In");
-            if (check)
+            else
             {
-                txtResult.Invoke(new MethodInvoker(delegate() {
-                    txtResult.Text += "Reg error\n";
-                }));
-               
-                return;
-            }
-            sendMess(http);
-            string log = fakerName + "|" + fakerEmail + "|" + fakerPassword;
-            writeLog(log);
-        }
-        void sendMess(Http http)
-        {
-            while (true)
-            {
-                HttpRequest requestSendMess = new HttpRequest();
-                requestSendMess.AddParam("comment", " hello hellohello hellohello hellohello hellohello hello hello hello hello ");
-                string idSeler = "ANSF0RE9FUP82";
-                HttpResponse responseLogin = http.PostUrlEncoded($"https://www.amazon.com/ss/help/contact/submitMessage?writeButton=Gửi+truy+vấn&subject=3&orderID=&sellerID={idSeler}&asin=&marketplaceID=ATVPDKIKX0DER&language=en_US", requestSendMess);
-                if (true)
+                string appActionToken = Regex.Match(htmlLogin, "name=\"appActionToken\" value=\"(.+?)\" />").Groups[1].Value.Trim();
+                string openidReturnTo = Regex.Match(htmlLogin, "name=\"openid.return_to\" value=\"(.+?)\" />").Groups[1].Value.Trim();
+                string prevRID = Regex.Match(htmlLogin, "name=\"prevRID\" value=\"(.+?)\" />").Groups[1].Value.Trim();
+                string workflowState = Regex.Match(htmlLogin, "name=\"workflowState\" value=\"(.+?)\" />").Groups[1].Value.Trim();
+
+                HttpRequest requestLogin = new HttpRequest();
+                requestLogin.AddParam("appActionToken", appActionToken);
+                requestLogin.AddParam("appAction", register);
+                requestLogin.AddParam("openid.return_to", openidReturnTo);
+                requestLogin.AddParam("prevRID	", prevRID);
+                requestLogin.AddParam("workflowState", workflowState);
+                requestLogin.AddParam("customerName", fakerName);
+                requestLogin.AddParam("email", fakerEmail);
+                requestLogin.AddParam("password", fakerPassword);
+                requestLogin.AddParam("passwordCheck", fakerPassword);
+                HttpResponse responseLogin = http.PostUrlEncoded(POST_URL_REGISTER, requestLogin);
+                if (responseLogin == null)
                 {
+                    PrintLog($"responseLogin {fakerEmail} error", Color.Red);
+                    return;
+                }
+                else
+                {
+                    string htmlLoginSuccess = http.QuickGetStr(URL_LOGIN_SUCCESS);
+                    if (String.IsNullOrEmpty(htmlLoginSuccess))
+                    {
+                        PrintLog($"htmlLoginSuccess {fakerEmail} error", Color.Red);
+                        return;
+                    }
+                    else
+                    {
+                        string checkEmailExists = Regex.Match(htmlLoginSuccess, "<title dir=\"ltr\">(.+?)</title>").Groups[1].Value.Trim();
+                        bool check = Equals(checkEmailExists, "Amazon Sign In");
+                        if (check)
+                        {
+                            PrintLog($"{fakerEmail} exists", Color.Linen);
+                            return;
+                        }
+                        SendMess(http, fakerEmail);
+                        string log = fakerName + "|" + fakerEmail + "|" + fakerPassword;
+                        WriteLog(log);
 
+                    }
                 }
             }
+        }
+        void SendMess(Http http, string mail)
+        {
+            txtListID.Invoke(new MethodInvoker(delegate ()
+            {
+                List<string> listID = CreateListFromRichTextBox(txtListID);
+                txtListMessage.Invoke(new MethodInvoker(delegate ()
+                {
+                    List<string> listMessages = CreateListFromRichTextBox(txtListMessage);
+                    foreach (var mess in listMessages)
+                    {
+                        foreach (var id in listID)
+                        {
+                            ThreadPool.QueueUserWorkItem((o) =>
+                            {
+                                HttpRequest requestSendMess = new HttpRequest();
+                                requestSendMess.AddParam("comment", mess);
+                                HttpResponse responseLogin = http.PostUrlEncoded($"https://www.amazon.com/ss/help/contact/submitMessage?writeButton=submit&subject=3&orderID=&sellerID={id}&asin=&marketplaceID=ATVPDKIKX0DER&language=en_US", requestSendMess);
+                                txtResult.Invoke(new MethodInvoker(delegate ()
+                                {
+                                    PrintLog($"{mail} send to {id} success", Color.Green);
+                                }));
+                            });
+                        }
+                    }
+                }));
+
+            }));
         }
         public Form1()
         {
             InitializeComponent();
-            //CheckForIllegalCrossThreadCalls = false;
         }
 
         private void btnClick_Click(object sender, EventArgs e)
         {
-            List<string> lstMessage = new List<string>(richTextBox1.Text.Split('\n'));
-
-            for (int i = 0; i < 8; i++)
-            {
-                ThreadStart ts = new ThreadStart(createAccount);
-                Thread th = new Thread(ts);
-                th.IsBackground = true;
-                th.Start();
-            }
-       
-            ////createAccount();
+            GetListSock();
         }
+
+        private void txtListSock_TextChanged(object sender, EventArgs e)
+        {
+            List<string> list = CreateListFromRichTextBox(txtListSock);
+            lblSockLive.Text = list.Count.ToString();
+        }
+
     }
 }
